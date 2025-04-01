f24f08f62356611441f1c40edfbb2209f0ec2a3f
이서윤 <w2318@e-mirim.hs.kr>
2025-04-01 22:21:16 +0900

    첫 페이지 CardMenu hover됨에 따라 backgroundColor, backgroundImage 변경

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Weather from "./assets/weather.png";
 import "./App.css";
 
 const cardData = [
-  { title: "오늘의 책 운세", imgSrc: Book },
-  { title: "오늘의 타로", imgSrc: Taro },
-  { title: "오늘의 포춘쿠키", imgSrc: Fortune },
-  { title: "오늘의 행운복권", imgSrc: Lucky },
-  { title: "오늘의 날씨운", imgSrc: Weather },
+  { title: "오늘의 책 운세", imgSrc: Book, backgroundImage: Book },
+  { title: "오늘의 타로", imgSrc: Taro, backgroundImage: Taro },
+  { title: "오늘의 포춘쿠키", imgSrc: Fortune, backgroundImage: Fortune },
+  { title: "오늘의 행운복권", imgSrc: Lucky, backgroundImage: Lucky },
+  { title: "오늘의 날씨운", imgSrc: Weather, backgroundImage: Weather },
 ];
 
 function CardMenu({ title, imgSrc, isHovered, onMouseEnter, onMouseLeave }) {
@@ -43,23 +43,46 @@ function App() {
     return () => clearInterval(interval);
   }, [isManualHover]);
 
+  const backgroundColors = [
+    "#BED5FF",
+    "#D6B7FF",
+    "#FFF9C7",
+    "#CCFFBF",
+    "#EBEBEB",
+  ];
+
   return (
-    <div className="main-container">
-      {cardData.map((card, index) => (
-        <CardMenu
-          key={index}
-          title={card.title}
-          imgSrc={card.imgSrc}
-          isHovered={hoverIndex === index}
-          onMouseEnter={() => {
-            setIsManualHover(true);
-            setHoverIndex(index);
-          }}
-          onMouseLeave={() => {
-            setIsManualHover(false);
-          }}
-        />
-      ))}
+    <div
+      className="main-container"
+      style={{
+        backgroundColor: backgroundColors[hoverIndex],
+        backgroundImage:
+          hoverIndex !== null
+            ? `url(${cardData[hoverIndex].backgroundImage})`
+            : "none",
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        transition: "background 0.5s ease-in-out",
+      }}
+    >
+      <div className="card-container">
+        {cardData.map((card, index) => (
+          <CardMenu
+            key={index}
+            title={card.title}
+            imgSrc={card.imgSrc}
+            isHovered={hoverIndex === index}
+            onMouseEnter={() => {
+              setIsManualHover(true);
+              setHoverIndex(index);
+            }}
+            onMouseLeave={() => {
+              setIsManualHover(false);
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
