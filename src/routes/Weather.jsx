9100195e5036d6698d820cf596e86c67b5b09c29
이서윤 <w2318@e-mirim.hs.kr>
2025-04-14 03:26:11 +0900

    Weather페이지 구조

--- src/routes/Weather.jsx
+++ src/routes/Weather.jsx
@@ -1,5 +1,30 @@
+import CategoryLayout from "../components/CategoryLayout";
+import WeatherImage from "../assets/weather.png";
+
+function WeatherAnimation() {
+  return (
+    <div className="weather-animation-container">
+      <div></div>
+    </div>
+  );
+}
+
+const backgroundColor = "#EBEBEB";
+const buttonColor = "#414141";
+const buttonHoverColor = "#535353";
+
 function Weather() {
-  return <div>Weather</div>;
+  return (
+    <CategoryLayout
+      imgSrc={WeatherImage}
+      animationComponent={<WeatherAnimation />}
+      categoryPhraseText="오늘의 날씨운 보기"
+      categoryButtonText="날씨운 보기"
+      backgroundColor={backgroundColor}
+      buttonColor={buttonColor}
+      buttonHoverColor={buttonHoverColor}
+    />
+  );
 }
 
 export default Weather;
