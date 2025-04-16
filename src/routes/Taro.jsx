4671857a6647d4139e138c0a3036a7900125b04a
leeseoyoun <w2318@e-mirim.hs.kr>
2025-04-16 12:17:07 +0900

    틀 잡기, 그라데이션 할 color 지정

--- src/routes/Taro.jsx
+++ src/routes/Taro.jsx
@@ -1,5 +1,32 @@
+import CategoryLayout from "../components/CategoryLayout";
+import TaroImage from "../assets/taro.png";
+
+function TaroAnimation() {
+  return (
+    <div className="taro-animation-container">
+      <div></div>
+    </div>
+  );
+}
+
+const backgroundColor = "#E4CFFF";
+const backgroundColor2 = "#C79CFF";
+const buttonColor = "#D468FF";
+const buttonHoverColor = "#cb49ff";
+
 function Taro() {
-  return <div>Taro</div>;
+  return (
+    <CategoryLayout
+      imgSrc={TaroImage}
+      animationComponent={<TaroAnimation />}
+      categoryPhraseText="오늘의 타로 확인하기"
+      categoryButtonText="타로 보기"
+      backgroundColor={backgroundColor}
+      backgroundColor2={backgroundColor2}
+      buttonColor={buttonColor}
+      buttonHoverColor={buttonHoverColor}
+    />
+  );
 }
 
 export default Taro;
