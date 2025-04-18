5ae5217b6be65563149a307dae430cf451e9ae02
이서윤 <w2318@e-mirim.hs.kr>
2025-04-19 02:31:25 +0900

    날씨에 맞는 문구 보여주기

--- src/routes/Weather.jsx
+++ src/routes/Weather.jsx
@@ -10,48 +10,52 @@ import React, { useState } from "react";
 import "../css/CategoryLayout.css";
 
 const weatherMap = {
-  "clear sky": sunny,
+  "clear sky": { image: sunny, phrase: "오늘의 날씨는 맑아요!" },
 
-  "light rain": rain,
-  "moderate rain": rain,
-  "heavy intensity rain": rain,
-  "very heavy rain": rain,
-  "extreme rain": rain,
-  "freezing rain": rain,
-  "light intensity shower rain": rain,
-  "shower rain": rain,
-  "heavy intensity shower rain": rain,
-  "ragged shower rain": rain,
-  "light intensity drizzle": rain,
-  drizzle: rain,
-  "heavy intensity drizzle": rain,
-  "light intensity drizzle rain": rain,
-  "drizzle rain": rain,
-  "heavy intensity drizzle rain": rain,
-  "shower rain and drizzle": rain,
-  "heavy shower rain and drizzle": rain,
-  "shower drizzle": rain,
+  "light rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "moderate rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "heavy intensity rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "very heavy rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "extreme rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "freezing rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "light intensity shower rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "shower rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "heavy intensity shower rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "ragged shower rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "light intensity drizzle": { image: rain, phrase: "오늘은 비가 와요." },
+  drizzle: { image: rain, phrase: "오늘은 비가 와요." },
+  "heavy intensity drizzle": { image: rain, phrase: "오늘은 비가 와요." },
+  "light intensity drizzle rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "drizzle rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "heavy intensity drizzle rain": { image: rain, phrase: "오늘은 비가 와요." },
+  "shower rain and drizzle": { image: rain, phrase: "오늘은 비가 와요." },
+  "heavy shower rain and drizzle": { image: rain, phrase: "오늘은 비가 와요." },
+  "shower drizzle": { image: rain, phrase: "오늘은 비가 와요." },
 
-  "broken clouds": blurry,
-  "overcast clouds": blurry,
+  "broken clouds": { image: blurry, phrase: "오늘은 흐린 날이에요." },
+  "overcast clouds": { image: blurry, phrase: "오늘은 흐린 날이에요." },
+  mist: { image: blurry, phrase: "오늘은 흐린 날이에요." },
 
-  "few clouds": littleBlurry,
-  "scattered clouds": littleBlurry,
+  "few clouds": { image: littleBlurry, phrase: "오늘은 구름이 조금 있어요." },
+  "scattered clouds": {
+    image: littleBlurry,
+    phrase: "오늘은 구름이 조금 있어요.",
+  },
 
-  "light snow": snow,
-  snow: snow,
-  "heavy snow": snow,
-  sleet: snow,
-  "light shower sleet": snow,
-  "shower sleet": snow,
-  "light rain and snow": snow,
-  "rain and snow": snow,
-  "light shower snow": snow,
-  "shower snow": snow,
-  "heavy shower snow": snow,
+  "light snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  snow: { image: snow, phrase: "오늘은 눈이 와요." },
+  "heavy snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  sleet: { image: snow, phrase: "오늘은 눈이 와요." },
+  "light shower sleet": { image: snow, phrase: "오늘은 눈이 와요." },
+  "shower sleet": { image: snow, phrase: "오늘은 눈이 와요." },
+  "light rain and snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  "rain and snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  "light shower snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  "shower snow": { image: snow, phrase: "오늘은 눈이 와요." },
+  "heavy shower snow": { image: snow, phrase: "오늘은 눈이 와요." },
 };
 
-function WeatherAnimation({ setResultShow }) {
+function WeatherAnimation({ setResultShow, setCategoryPhraseText }) {
   const [input, setInput] = useState("");
   const [weather, setWeather] = useState("");
   const [showInput, setShowInput] = useState(true);
@@ -71,19 +75,22 @@ function WeatherAnimation({ setResultShow }) {
       `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=792ff3066b1a91e7e54aabf9de16f2ee&units=metric&lang=en`
     )
       .then((res) => {
-        if (!res.ok) throw new Error("City not found");
+        if (!res.ok) throw new Error("정확한 도시명을 입력해 주세요.");
         return res.json();
       })
       .then((data) => {
         const original = data.weather[0].description;
+        console.log(data.weather);
         const mapped = weatherMap[original];
 
         setTimeout(() => {
           if (mapped) {
             setWeather(mapped);
+            setCategoryPhraseText(mapped.phrase);
             setResultShow(true);
           } else {
             setWeather("unsupported");
+            setCategoryPhraseText("날씨를 알 수 없어요.");
           }
           setResultShow(true);
           setLoading(false);
@@ -103,19 +110,8 @@ function WeatherAnimation({ setResultShow }) {
       setWeather("");
       setResultShow(false);
       setInput("");
+      setCategoryPhraseText("오늘의 날씨운 보기");
     }, 1);
-  };
-
-  const unsupported = () => {
-    if (weather === "unsupported") return <p>지원하지 않는 날씨에요</p>;
-    return (
-      <img
-        className="image"
-        src={weather}
-        alt="weather"
-        style={{ height: "80%", width: "auto" }}
-      />
-    );
   };
 
   return (
@@ -139,7 +135,14 @@ function WeatherAnimation({ setResultShow }) {
 
       {loading && <p>운세 알아보는 중...</p>}
 
-      {!loading && weather && unsupported()}
+      {!loading && weather && (
+        <img
+          className="image"
+          src={weather.image}
+          alt="weather"
+          style={{ height: "80%", width: "auto" }}
+        />
+      )}
     </div>
   );
 }
@@ -151,12 +154,19 @@ const buttonHoverColor = "#747474";
 
 function Weather() {
   const [resultShow, setResultShow] = useState(false);
+  const [categoryPhraseText, setCategoryPhraseText] =
+    useState("오늘의 날씨운 보기");
 
   return (
     <CategoryLayout
       imgSrc={WeatherImage}
-      animationComponent={<WeatherAnimation setResultShow={setResultShow} />}
-      categoryPhraseText="오늘의 날씨운 보기"
+      animationComponent={
+        <WeatherAnimation
+          setResultShow={setResultShow}
+          setCategoryPhraseText={setCategoryPhraseText}
+        />
+      }
+      categoryPhraseText={categoryPhraseText}
       categoryButtonText="날씨운 보기"
       backgroundColor={backgroundColor}
       backgroundColor2={backgroundColor2}
