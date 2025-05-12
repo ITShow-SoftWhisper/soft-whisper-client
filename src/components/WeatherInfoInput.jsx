c74169680ea2b2b914c80c167dac3f03025b021f
leeseoyoun <w2318@e-mirim.hs.kr>
2025-05-12 09:27:10 +0900

    커서 자동포커스, Enter키 눌러도 onSubmit

--- src/components/WeatherInfoInput.jsx
+++ src/components/WeatherInfoInput.jsx
@@ -1,7 +1,12 @@
-import React from "react";
 import "../css/WeatherInfoInput.css";
 
 function WeatherInfoInput({ input, setInput, onSubmit, isFadingOut }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div className={`container ${isFadingOut ? "fade-out" : ""}`}>
       <div className="weather-info-input">
@@ -11,6 +16,8 @@ function WeatherInfoInput({ input, setInput, onSubmit, isFadingOut }) {
           placeholder="도시 이름을 입력하세요 (예 : 서울특별시)"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+          autoFocus
         />
         <button className="submit-button jua-regular" onClick={onSubmit}>
           운세 보기
